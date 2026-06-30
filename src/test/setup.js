27697a47bffd37b5cbf084aad77705a6a3ecf385
zelkirb <russklimas@gmail.com>
2026-06-29 18:19:53 -0600

    test: add Vitest + Testing Library + Playwright tooling

---
+++ src/test/setup.js
@@ -0,0 +1,15 @@
+import '@testing-library/jest-dom';
+
+// jsdom lacks matchMedia; default to "no reduced motion".
+if (!window.matchMedia) {
+  window.matchMedia = (query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addEventListener: () => {},
+    removeEventListener: () => {},
+    addListener: () => {},
+    removeListener: () => {},
+    dispatchEvent: () => false,
+  });
+}
